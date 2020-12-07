2c0a6b6f73c7611408590df9f844024a6f6235c0
Jessica Gage <jemgage@gmail.com>
2020-12-07 14:50:45 -0700

    Made some changes to switch to using restify.serve() rather than manually configuring every route.

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const restify = require('restify');
+const express = require('express');
+const restify = require('express-restify-mongoose');
+const clientModel = require("./models/Client");
+const bodyParser = require('body-parser');
+const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config');
 
-const server = restify.createServer();
+const app = express();
+app.use(bodyParser.json({limit: "10gb"}));
+app.use(express.json());
 
 //Middleware
 
-server.use(restify.plugins.bodyParser());
-
-server.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
     mongoose.connect(config.MONGODB_URI, 
         { useNewUrlParser: true, useUnifiedTopology: true } 
     );
@@ -19,8 +23,10 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 
 db.once('open', () => {
-    require('./routes/crud')(server);
+    restify.serve(router, clientModel);
+    app.use(router);
+
     console.log(`server started on port ${config.PORT}`);
 });
 
-module.exports = server
+module.exports = app;
