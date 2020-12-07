d9e95fede1099ef7deba17a5e6277a03db4a83c4
Jessica Gage <jemgage@gmail.com>
2020-12-07 15:07:42 -0700

    Tested my own API to ensure that /api/v1/client returns an empty [], which it does. Fully-passing test suite.

--- test/ownAPI.test.js
+++ test/ownAPI.test.js
@@ -1,21 +1,19 @@
+const request = require('request-promise');
 const chai = require('chai');
-const chai_http = require('chai-http');
+const chaiHttp = require('chai-http');
 
-var chaiApp = require('../app.js');
-chai.use(chai_http);
+var app = require('../app.js');
+var expect = require('chai').expect;
+chai.use(chaiHttp);
 
-chai.request(chaiApp)
-    .get('/clients')
-    .end(async (error, response) => {
-        if(error){
-            console.log(error);
-        }else{
-            chai.assert.equal(response.status, 200, 
-                'Response was not ok');
-            chai.assert.equal(response.text, [], 
-                'Response did not return ok');
-            
-            console.log("All tests in chai suite passing");
-            return
-        }
-    });
+describe("#request", async function () {
+    it("pulls data from GET request to parse info to usable data",
+    async function (){
+        const response = await request.get({url: 'http://localhost:8000/api/v1/client', 
+            json: true});
+
+        console.log(response);
+
+        expect(response).to.eql([]);
+    })
+})
