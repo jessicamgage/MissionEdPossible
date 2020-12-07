2256fac7921ce0d4668b040c622cbcd3584d915f
Jessica Gage <jemgage@gmail.com>
2020-12-07 15:16:23 -0700

    Posting test logic for a mock test for review.

--- test/ownAPI.test.js
+++ test/ownAPI.test.js
@@ -8,12 +8,26 @@ chai.use(chaiHttp);
 
 describe("#request", async function () {
     it("pulls data from GET request to parse info to usable data",
-    async function (){
+    async function () {
         const response = await request.get({url: 'http://localhost:8000/api/v1/client', 
             json: true});
 
         console.log(response);
 
         expect(response).to.eql([]);
+    })
+})
+
+describe("#request", async function () {
+    it("uses mocking to set up an instance of API, pushes data to it, then tears it down after completion",
+    async function () {
+        //helper function to set up 
+
+        //post request to []
+
+        const response = await request.get({url: 'http://localhost:8000/api/v1/client',
+            json: true});
+
+        //helper function to tear down
     })
 })
